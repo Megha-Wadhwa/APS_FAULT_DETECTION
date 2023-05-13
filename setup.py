d92665ab588ebf7e997b5a856ca79cb80b8751e3
megha <meghaw42@gmail.com>
2023-05-14 00:00:19 +0530

    Create inital setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+def get_requirements():
+    pass
+
+setup(
+    name="sensor",
+    version="0.0.1"
+    author="tbd"
+    author_email="meghaw42@gmail.com",
+    packages = find_packages(),
+    install_reuires=get.requirements(),
+)
