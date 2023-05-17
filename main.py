b86ef2d1bbd0e8c1615811f75e911daee1162dc7
megha <meghaw42@gmail.com>
2023-05-18 00:37:25 +0530

    DataIngestion

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+from sensor.exception import SensorException
+from sensor.logger import logging
+from sensor.utils import get_collection_as_dataframe
+import sys,os
+from sensor.entity import config_entity
+from sensor.components import data_ingestion
 
-from sensor.pipeline.training_pipeline import start_training_pipeline
-from sensor.pipeline.batch_prediction import start_batch_prediction
-
-file_path="/config/workspace/aps_failure_training_set1.csv"
-print(__name__)
 if __name__=="__main__":
      try:
-          #start_training_pipeline()
-          output_file = start_batch_prediction(input_file_path=file_path)
-          print(output_file)
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config= config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion = data_ingestion.DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
      except Exception as e:
           print(e)
--- sensor/logger.py
+++ sensor/logger.py
@@ -7,8 +7,9 @@ import os
 LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
 
 #log directory
-LOG_FILE_DIR = os.path.join(os.getcwd(),"logs"
+LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
 
+#create folder if not available
 os.makedirs(LOG_FILE_DIR,exist_ok=True)
 
 #log file path
