89fa94af7a7ed4bb5c70259f1482aa07253f68a6
megha <meghaw42@gmail.com>
2023-05-21 13:31:46 +0530

    Data Validation done

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sensor.utils import get_collection_as_dataframe
 import sys,os
 from sensor.entity import config_entity
 from sensor.components import data_ingestion
+from sensor.components.data_validation import DataValidation
 
 if __name__=="__main__":
      try:
@@ -11,6 +12,16 @@ if __name__=="__main__":
           data_ingestion_config= config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
           print(data_ingestion_config.to_dict())
           data_ingestion = data_ingestion.DataIngestion(data_ingestion_config=data_ingestion_config)
-          print(data_ingestion.initiate_data_ingestion())
+          data_ingestion_artifact= data_ingestion.initiate_data_ingestion()
+
+          data_validation_config =config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+          data_validation=DataValidation(data_validation_config=data_validation_config,
+                         data_ingestion_artifact= data_ingestion_artifact)
+
+        
+
+          data_validation_artifact = data_validation.initiate_data_validation()
+
+          
      except Exception as e:
           print(e)
--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -1,11 +1,15 @@
 from dataclasses import dataclass
 
+@dataclass
 class DataIngestionArtifact:
     feature_store_file_path:str
     train_file_path:str
     test_file_path:str
+
+@dataclass   
+class DataValidationArtifact:
+    report_file_path:str
     
-class DataValidationArtifact:...
 class DataTransformationArtifact:...
 class ModelTrainerArtifact:...
 class ModelEvaluationArtifact:...
